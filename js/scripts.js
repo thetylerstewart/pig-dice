6a4cde4db3c44fe3bc6ef9798e429e2efb1e0d12
Tyler Stewart <mr.tstew@gmail.com>
2022-04-30 15:19:07 -0700

    add win check

--- js/scripts.js
+++ js/scripts.js
@@ -3,39 +3,99 @@ function PlayerOne() {
 }
 
 PlayerOne.prototype.diceRoll = function () {
+  $('#one').hide()
+  $('#two').hide()
+  $('#three').hide()
+  $('#four').hide()
+  $('#five').hide()
+  $('#six').hide()
   let roll = Math.floor(Math.random() * 6) + 1
   if (roll === 1) {
+    $('#one-one').show()
     this.score = 0
     $('#player-one').hide()
     $('#player-two').show()
-  } else {
+    $('#one').show()
+  } else if (roll === 2) {
+    $('#two').show()
+    this.score += roll
+    return this.score
+  } else if (roll === 3) {
+    $('#three').show()
+    this.score += roll
+    return this.score
+  } else if (roll === 4) {
+    $('#four').show()
+    this.score += roll
+    return this.score
+  } else if (roll === 5) {
+    $('#five').show()
+    this.score += roll
+    return this.score
+  } else if (roll === 6) {
+    $('#six').show()
     this.score += roll
     return this.score
   }
 }
+
+PlayerOne.prototype.winner = function () {
+  if (this.score >= 100) {
+    $('#player-one-win').show();
+    $('#player-one').hide()
+    $('#player-two').hide()
+  }
+}
+
 
 
 function PlayerTwo() {
   this.score = 0;
 }
-
 PlayerTwo.prototype.diceRoll = function () {
+  $('#one').hide()
+  $('#two').hide()
+  $('#three').hide()
+  $('#four').hide()
+  $('#five').hide()
+  $('#six').hide()
   let roll = Math.floor(Math.random() * 6) + 1
   if (roll === 1) {
     this.score = 0
     $('#player-two').hide()
     $('#player-one').show()
-
-  } else {
+    $('#one').show()
+  } else if (roll === 2) {
+    $('#two').show()
+    this.score += roll
+    return this.score
+  } else if (roll === 3) {
+    $('#three').show()
+    this.score += roll
+    return this.score
+  } else if (roll === 4) {
+    $('#four').show()
+    this.score += roll
+    return this.score
+  } else if (roll === 5) {
+    $('#five').show()
+    this.score += roll
+    return this.score
+  } else if (roll === 6) {
+    $('#six').show()
     this.score += roll
     return this.score
   }
+
 }
 
-
-
-
-
+PlayerTwo.prototype.winner = function () {
+  if (this.score >= 100) {
+    $('#player-two-win').show();
+    $('#player-one').hide()
+    $('#player-two').hide()
+  }
+}
 
 //User Interface
 let newPlayerOne = new PlayerOne()
@@ -48,6 +108,7 @@ $(document).ready(function () {
     event.preventDefault()
     newPlayerOne.diceRoll()
     $('.player-one-score').html(newPlayerOne.score)
+    newPlayerOne.winner()
   })
 
   $('#player-one-hold').click(function (event) {
@@ -57,13 +118,13 @@ $(document).ready(function () {
   })
 
 
-
   // ------ PLAYER TWO -------
 
   $('#player-two-roll').click(function (event) {
     event.preventDefault()
     newPlayerTwo.diceRoll()
     $('.player-two-score').html(newPlayerTwo.score)
+    newPlayerTwo.winner()
   })
 
   $('#player-two-hold').click(function (event) {
@@ -71,5 +132,4 @@ $(document).ready(function () {
     $('#player-two').hide()
     $('#player-one').show()
   })
-
 })
